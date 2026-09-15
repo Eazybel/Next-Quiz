daaa8436f698832295ab85f04047e7780678a514
Eazybel <eazybel27@gmail.com>
2026-09-16 00:58:26 +0300

    refactor: update CategoryNav component for improved state handling and code clarity

--- quizapp/src/app/components/CategoryNav/CategoryNav.tsx
+++ quizapp/src/app/components/CategoryNav/CategoryNav.tsx
@@ -1,34 +1,34 @@
 "use client"
-
-type textProps={
-   text:string[]
+type textProp={
+    text:string[]
 }
-import React from "react";
-
 import {useState} from "react"
 import {useRouter} from "next/navigation"
-export default function CategoryNav(props:textProps){
-    const router=useRouter()
-    const [change,setChange] = useState("")
-    const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+export default function CategoryNav(props:textProp){
+const router=useRouter()
+const [change,setChange]=useState("string")
+const changeHandler=(e:React.ChangeEvent<HTMLInputElement>)=>{
+
 setChange(e.target.value)
-    }
-    const clickHandler=(e:React.FormEvent)=>{
-        e.preventDefault()
-        console.log(change)
-router.push(`Questions/${change}`)
-    }
+}
+const clickHandler=(e:React.SubmitEvent)=>{
+e.preventDefault()
+router.push(`/questions/${change}`)
+}
     return(
-        <form action="">
-            {
-                props.text.map((inputs:string,index:number)=>{
-                    return <>
-                    <label key={index} htmlFor={inputs}>{inputs}</label>
-                    <input value={inputs} onChange={handleChange} key={inputs} type="radio" name="category" id={`${inputs}`} placeholder={inputs}/>
-                    </>
-                })
-            }
-            <button onClick={clickHandler} type="submit">Submit</button>
+        <>
+        <form>
+        {
+            props.text.map((inputs:string,index:number)=>{
+                return <>
+                <label key={index} htmlFor={inputs}></label>
+                <input key={index} onChange={changeHandler} name="category" type="radio" value={inputs} /><br />
+
+                        </>
+            })
+        }
+        <button type="submit">Submit</button>
         </form>
+        </>
     )
 }

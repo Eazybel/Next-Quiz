f298c83f26f1961bf2aa6bdb511b9bf75bd597fd
Eazybel <eazybel27@gmail.com>
2026-09-15 21:19:42 +0300

    feat: replace navigation links with radio buttons for category selection in CategoryNav component

--- quizapp/src/app/components/CategoryNav/CategoryNav.tsx
+++ quizapp/src/app/components/CategoryNav/CategoryNav.tsx
@@ -1,11 +1,20 @@
-import Link from "next/link"
+import React from "react";
+import {useState} from "react"
 export default function CategoryNav(){
+    const [change,setChange] = useState("")
+    const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
+setChange(e.target.value)
+    }
+    const clickHandler=(e:React.FormEvent)=>{
+console.log(change)
+    }
     return(
-        <nav>
-        <Link href={"/Questions"}>Front end</Link><br />
-        <Link  href={"/Questions"}>Back end</Link><br />
-        <Link  href={"/Questions"}>Full stack</Link><br />
-
-        </nav>
+        <form action="">
+            <input onChange={handleChange} type="radio" name="categoryy" id="chemistry" >chemistry</input>
+            <input onChange={handleChange} type="radio" name="categoryy" id="biology" >biology</input>
+            <input onChange={handleChange} type="radio" name="categoryy" id="history" >history</input>
+            <input onChange={handleChange} type="radio" name="categoryy" id="math" >math</input>
+            <button onClick={clickHandler} type="submit">Submit</button>
+        </form>
     )
 }

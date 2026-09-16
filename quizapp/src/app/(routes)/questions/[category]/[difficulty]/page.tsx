060c3bc3c803378ae0f0a23471292b17cb5ea6f7
Eazybel <eazybel27@gmail.com>
2026-09-16 21:36:08 +0300

    refactor: update Question component to display selected category and difficulty, and integrate FetchCategory functionality

--- quizapp/src/app/(routes)/questions/[category]/[difficulty]/page.tsx
+++ quizapp/src/app/(routes)/questions/[category]/[difficulty]/page.tsx
@@ -1,15 +1,14 @@
 "use client"
 import {useParams} from "next/navigation"
-import {useState} from "react"
-import FetchCategory from "@/app/api/fetchCategory"
+
 
 export default function Question(){
     const paramsItem=useParams<{category:string,difficulty:string}>()
 
     return(
         <>
-        <p>catagorys are listed below</p>
-        <FetchCategory/>
+        <p>you selected {paramsItem.category} of {paramsItem.difficulty} level questions</p>
+
         </>
     )
 }
--- quizapp/src/app/api/fetchCategory.tsx
+++ quizapp/src/app/api/fetchCategory.tsx
@@ -1,10 +1,12 @@
 "use client"
 import React, {useState,useEffect} from "react"
+import {useRouter} from "next/navigation"
 type Category={
     name:string
 }
 
 export default function FetchCategory(){
+    const router=useRouter()
     const [category,setCategory]=useState<Category[]>([])
     const [isLoading,setLoad]=useState(true)
     const [error,setError]=useState<string|null>()
@@ -22,7 +24,7 @@ export default function FetchCategory(){
     }
     const clickHandler=(e:React.SubmitEvent<HTMLFormElement>)=>{
                 e.preventDefault()
-         console.log(level,categoryType)
+         router.push(`/questions/${categoryType}/${level}`)
     }
     useEffect(()=>{
     fetch("https://quizapi.io/api/v1/categories")

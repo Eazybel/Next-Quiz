373890f6933104dda244f489460fd23ac275d2cc
Eazybel <eazybel27@gmail.com>
2026-09-17 20:06:23 +0300

    refactor: update Question component to fetch and display questions based on selected category and difficulty, and enhance FetchCategory with improved level options

--- quizapp/src/app/(routes)/questions/[category]/[difficulty]/page.tsx
+++ quizapp/src/app/(routes)/questions/[category]/[difficulty]/page.tsx
@@ -1,12 +1,56 @@
-"use client"
-import {useState,useEffect} from "react"
-import {useParams}from "next/navigation"
-export default  function Question(){
-   const {category,difficulty}=useParams<{category:string,difficulty:string}>()
-    return(
-        <>
-        <p>you selected {category} of {difficulty} level questions</p>
+type paramsType={
+    params:Promise<{category:string,difficulty:string}>
+}
+type answersType={
+isCorrect:boolean,
+text:string
+}
+type questionType={
+    answers:answersType[],
+    explanation:string,
+    text:string,
 
-        </>
-    )
+}
+type clickDataType={
+    question:string,
+    answer:string
+}
+export default async function Question({params}:paramsType){
+const paramsItem=await params
+const res =await fetch(`https://quizapi.io/api/v1/questions?category=${paramsItem.category}&difficulty=${paramsItem.difficulty}&type=MULTIPLE_CHOICE&limit=10&offset=0"`,{method:"Get",headers:{"Content-type":"application/json","Authorization":"Bearer qa_sk_84cb0450a91a0e65b7e4461ac4df9decb89b74d2"}
+    })
+const data=await res.json()
+const fetchedData=data.data
+console.log(fetchedData)
+if(!res.ok){
+    console.log(res)
+    throw Error("Something went wwrong")
+}
+const clickData:clickDataType[]=[]
+return(
+    <>
+    {
+       <div>
+        { fetchedData.map((questions:questionType,index:number)=>{
+            const currentQuesition=questions.text
+            let currentAnswer=""
+            questions.answers.forEach(q=>{if(q.isCorrect){currentAnswer=q.text}})
+            return <div key={index}>
+                    <div><p>{index+1}</p><h2>{questions.text}</h2></div>
+                    {questions.answers.map((answer,index)=>{
+                            
+                        return <div key={index}>
+                            <input  type="radio" name="choose" id={answer.text} value={answer.text}/>
+                            <label htmlFor={answer.text}>{answer.text}</label>
+                        </div>
+                    })}
+        {clickData.push({question:currentQuesition,answer:currentAnswer})}
+                    </div>
+        })}
+        {console.log(clickData)}
+       </div>
+    }
+    </>
+)
+
 }
--- quizapp/src/app/api/fetchCategory.tsx
+++ quizapp/src/app/api/fetchCategory.tsx
@@ -15,16 +15,15 @@ export default function FetchCategory(){
     const [isOpen,setOpen]=useState(false)
     const changeHandlerCategory=(e:React.ChangeEvent<HTMLFormElement>)=>{
         setCategoryType(e.target.value)
-        localStorage.setItem("category",JSON.stringify(category.filter(name=>{return name.name===e.target.value})))
         setOpen(true)
     }
     const changeHandlerLevel=(e:React.ChangeEvent<HTMLFormElement>)=>{
             setLevel(e.target.value)
     }       
     const clickHandler=(e:React.SubmitEvent<HTMLFormElement>)=>{
-                e.preventDefault()
+        e.preventDefault()
                 
-         router.push(`/questions/${categoryType}/${level}`)
+        router.push(`/questions/${categoryType}/${level}`)
     }
     useEffect(()=>{
     fetch("https://quizapi.io/api/v1/categories")
@@ -64,12 +63,14 @@ export default function FetchCategory(){
             }
             {
                 isOpen&& <form onSubmit={clickHandler} onChange={changeHandlerLevel}>
-                <label htmlFor="easy">easy</label>
-                <input required type="radio" name="level" id="easy" value="easy"/><br/>
-                <label htmlFor="medium">medium</label>
-                <input required type="radio" name="level" id="medium" value="medium"/><br/>
-                <label htmlFor="hard">hard</label>
-                <input required type="radio" name="level" id="hard" value="hard"/>
+                <label htmlFor="easy">EASY</label>
+                <input required type="radio" name="level" id="EASY" value="EASY"/><br/>
+                <label htmlFor="medium">MEDIUM</label>
+                <input required type="radio" name="level" id="MEDIUM" value="MEDIUM"/><br/>
+                <label htmlFor="hard">HARD</label>
+                <input required type="radio" name="level" id="HARD" value="HARD"/><br />
+                <label htmlFor="hard">EXPERT</label>
+                <input required type="radio" name="level" id="EXPERT" value="EXPERT"/><br />
                 <button type="submit">Submit</button>
             </form>
             }

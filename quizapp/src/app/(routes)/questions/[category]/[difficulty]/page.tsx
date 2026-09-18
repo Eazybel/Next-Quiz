978d3524618ea1101cdecb1c2bb8f5ce83b6ca11
Eazybel <eazybel27@gmail.com>
2026-09-18 20:47:25 +0300

    refactor: enhance SingleTest component by improving question rendering and adding styled Submit button

--- quizapp/src/app/(routes)/questions/[category]/[difficulty]/page.tsx
+++ quizapp/src/app/(routes)/questions/[category]/[difficulty]/page.tsx
@@ -1,3 +1,6 @@
+import Question from '@/app/components/Question/Question'
+import Choose from '@/app/components/Choose/Choose'
+import Submit from '@/app/components/Submit/Submit'
 type paramsType={
     params:Promise<{category:string,difficulty:string}>
 }
@@ -10,8 +13,6 @@ type ChooseType={
     id:string,
     isCorrect:boolean
 }
-import Question from '@/app/components/Question/Question'
-import Choose from '@/app/components/Choose/Choose'
 export default async function SingleTest({params}:paramsType){
 const paramsItem=await params
 const res =await fetch(`https://quizapi.io/api/v1/questions?category=${paramsItem.category}&difficulty=${paramsItem.difficulty}&type=MULTIPLE_CHOICE&limit=10&offset=0"`,{method:"Get",headers:{"Content-type":"application/json","Authorization":"Bearer qa_sk_84cb0450a91a0e65b7e4461ac4df9decb89b74d2"}
@@ -22,36 +23,26 @@ const fetchedData=data.data
 if(!res.ok){
     throw Error("Something went wwrong")
 }
-return(
-
-       <>
-       {
-        fetchedData.map((questions:QuestionsType,index:number)=>{
-                return <div>
-                     <div key={index+1} className="flex">
-                          <h1>{index+1}</h1><Question text={questions.text} key={index+1}/> 
-                        </div><br />
-                        
-                            <div>
-                               { questions.answers.map((choose:ChooseType,index:number)=>{
-                                        return (
-                                            
-                                            <div key={index}>
-                                                <Choose text={choose.text}/>
-                                            </div>
-                                            
+return(<>
+ {
+     fetchedData.map((questions:QuestionsType,index:number)=>{
+        return <div>
+            <div key={index+1} className="flex">
+                <h1>{index+1}</h1><Question text={questions.text} key={index+1}/> 
+                    </div><br />
+                        <div>
+                            { questions.answers.map((choose:ChooseType,index:number)=>{
+                                 return (  
+                                    <div key={index}>
+                                        <Choose text={choose.text}/>
+                                        </div>    
                                         )
                                 })}
-                            </div><br />
-                            
-                        
+                            </div><br />                    
                  </div>
-
-
             })
-       }
-       </>
-    
-)
+       } 
+    <Submit/>     
+</>)
 
 }
